1eff25c80cb255ece8acda0f0ce2b1c32c51feec
AlekseySemukov <ches488@gmail.com>
2023-04-11 23:37:46 +0700

    add nav

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { connect, Provider } from "react-redux";
+import Header from './Components/Header/Header'
+import Navbar from './Components/Navbar/Navbar'
+import Monitoring from './Components/Monitoring/Monitoring'
+import store from './redux/store'
 
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Root />,
+    },
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Provider store={store}>
+        <div className="app-wrapper">
+          <Header />
+          <Navbar />
+          <div className="app-wrapper-content">
+            <section>
+              <Routes>
+                <Route exact path="/" element={<Navigate to="/monitoring" />} />
+                <Route path='/profile/*' element={<Monitoring />} />
+                <Route path='/monitoring/*' element={<Monitoring />} />
+                <Route path='/setting-device/*' element={<Monitoring />} />
+                <Route path='/setting/*' element={<Monitoring />} />
+                <Route path='*' element={<div>404 NOT FOUND</div>} />
+              </Routes>
+            </section>
+          </div>
+        </div>
+      </Provider>
+    </BrowserRouter>
+
   );
 }
 
 export default App;
+
+{/* // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div> */}
